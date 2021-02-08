6e242afd652659293d14222e7cd8553afe8f2a9b
Deserie Murembeni <deserie.dev@gmail.com>
2021-02-08 18:26:54 +0200

    fix: review errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,25 @@
-const profile = document.querySelector(".profile");
-const edit = document.querySelector(".profile__edit");
 const modal = document.querySelector(".modal");
-const form = modal.querySelector(".modal__form");
-const openModalButton = document.querySelector(".profile__edit");
+const edit = document.querySelector(".profile__edit");
+const form = document.querySelector(".modal__form");
 const closeForm = modal.querySelector(".modal__close-button");
 
-//Queries to modify content of profileName and profileOccupation// 
 const formName = form.querySelector(".modal__form-control_input_name");
 const formOccupation = form.querySelector(".modal__form-control_input_occupation");
-const profileName = document.querySelector('.profile__name');
-const profileOccupation = document.querySelector('.profile__occupation')
+const profileName = document.querySelector(".profile__name");
+const profileOccupation = document.querySelector(".profile__occupation")
 
 function openModal() {
+  console.log("You have clicked on the element");
   modal.classList.add("modal_opened");
   formName.value = profileName.textContent;
   formOccupation.value = profileOccupation.textContent;
 }
 
-function closeModal(){
-  modal.classList.remove("modal_closed");
+function closeModal() {
+  modal.classList.remove("modal_opened");
 }
 
-//On click run the function openModal() to open the modal//
 edit.addEventListener("click", openModal);
-// On click run the function closeModal() to close the modal//
 closeForm.addEventListener("click", closeModal);
 
 function saveProfile(){
