838e76e0a6b0487366248987cc67ffe7b60990d5
Deserie Murembeni <deserie.dev@gmail.com>
2021-02-21 14:11:21 +0200

    feature: new place card form

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,47 @@ const edit = document.querySelector(".profile__edit");
 const form = document.querySelector(".modal__form");
 const closeForm = modal.querySelector(".modal__close-button");
 
+
+//Variables for the edit profile section
 const formName = form.querySelector(".modal__form-control_input_name");
 const formOccupation = form.querySelector(".modal__form-control_input_occupation");
+const editForm = document.querySelector(".edit-form");
 const profileName = document.querySelector(".profile__name");
 const profileOccupation = document.querySelector(".profile__occupation");
 
+
+//Variables for the add new card/place section
+const addButton = document.querySelector(".profile__add");
+const createForm = document.querySelector(".create-form");
+const formTitle = form.querySelector(".modal__form-control_input_title");
+const formImage = form.querySelector(".modal__form-control_input_image");
+const placeCloseButton = document.querySelector(".modal__close-button_place");
+
+
+//Function to open modal
 function openModal() {
   modal.classList.add("modal_opened");
-  formName.value = profileName.textContent;
-  formOccupation.value = profileOccupation.textContent;
 }
 
+
+//Function to close modal
 function closeModal() {
   modal.classList.remove("modal_opened");
 }
 
-edit.addEventListener("click", openModal);
+//When edit button clicked, open "Edit Profile" modal. Values for each input field.
+edit.addEventListener("click", function() {
+  openModal();
+  editForm.classList.add("modal_opened");
+  formName.value = profileName.textContent;
+  formOccupation.value = profileOccupation.textContent;
+});
+
+// Close the modal when the close button is clicked.
 closeForm.addEventListener("click", closeModal);
 
+
+//Update site when text entered into input fields then close the popup.
 function saveProfile() {
   profileName.textContent = formName.value;
   profileOccupation.textContent = formOccupation.value;
@@ -29,3 +52,13 @@ function saveProfile() {
 }
 
 form.addEventListener("submit", saveProfile);
+
+
+//Create new place functionality
+addButton.addEventListener("click", function() {
+  createForm.classList.add("modal_opened");  
+  formTitle.value = "";
+  formImage.value = "";
+});
+
+placeCloseButton.addEventListener("click", closeModal);
